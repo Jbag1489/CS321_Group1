28c09555ab8e1967b2925831500369ba3811d0b5
Dina Brown <dsb1492@gmail.com>
2016-04-10 15:39:16 -0500

    Resubmitting Assignment.java

--- 321_Group_Project/src/cs321/other/Assignment.java
+++ 321_Group_Project/src/cs321/other/Assignment.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * The Assignment class will implement the interface of the assignment using 
  * GUI component. This class will also display instructions and the assignment 
@@ -75,76 +75,4 @@ public class Assignment {
         this.userEnteredCode = userEnteredCode;
     }
 }
-=======
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package cs321.other;
-//
-///**
-// *
-// * @author jdbrown2005
-// */
-//public class Assignment {
-//    //Fields:
-//    //Can go back and change the types when meet as a group
-//    //Not sure about *assignmentParser ??
-//    private String[] instructions = new String[0];
-//    private String[] masterCode = new String[0];
-//    private String[] userEnteredCode = new String[0];
-//    //assignmentParser?
-//    
-//    
-//    //Methods:
-//    
-//    /**
-//     * JavaDoc comment
-//    */
-//    public String[] getInstructions()
-//    {
-//        return instructions;
-//    }
-//    
-//    /**
-//     * JavaDoc comment
-//    */
-//    public String[] setInstruction()
-//    {
-//        //??
-//    }
-//    
-//    /**
-//     * JavaDoc comment
-//    */
-//    public String[] getMasterCode()
-//    {
-//        return masterCode;
-//    }
-//    
-//    /**
-//     * JavaDoc comment
-//    */
-//    public String[] setMasterCode()
-//    {
-//        //??
-//    }
-//    
-//    /**
-//     * JavaDoc comment
-//    */
-//    public String[] getUserEnteredCode()
-//    {
-//        return userEnteredCode;
-//    }
-//    
-//    /**
-//     * JavaDoc comment
-//    */
-//    public String[] setUserEnteredCode()
-//    {
-//        //??
-//    }
-//}
->>>>>>> origin/master
+
