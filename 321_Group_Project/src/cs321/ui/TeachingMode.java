65c207071ceebd74a43dc57787df3ef94323d7f9
Josh Bagwell <Jbag1489@gmail.com>
2016-04-18 00:59:02 -0500

    Delete generated main methods from GUI windows

--- 321_Group_Project/src/cs321/ui/TeachingMode.java
+++ 321_Group_Project/src/cs321/ui/TeachingMode.java
@@ -248,10 +248,7 @@ public class TeachingMode extends javax.swing.JFrame {
 
     private void ExitMainMenuButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ExitMainMenuButtonActionPerformed
         // TODO add your handling code here:
-
         this.setVisible(false);
-
-
     }//GEN-LAST:event_ExitMainMenuButtonActionPerformed
 
     private void SubmitButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SubmitButtonActionPerformed
@@ -295,43 +292,6 @@ public class TeachingMode extends javax.swing.JFrame {
         // TODO add your handling code here:
         aboutMenu.setVisible(true);
     }//GEN-LAST:event_AboutMenuItemActionPerformed
-
-    /**
-     * The main method of the class TeachingMode.
-     *
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TeachingMode.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TeachingMode.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TeachingMode.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TeachingMode.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new TeachingMode().setVisible(true);
-            }
-        });
-    }
 
     private void moveTypedCodeBackToArrayList() {
         String s[] = typedCodeTextArea.getText().split("\\n");
